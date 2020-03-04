b7e7c69cfc0a269825700dae7e3832fee4e5764c
cxy004 <cxy004@qq.com>
2020-03-04 21:30:38 +0800

    subworkdir 2020/03/04 21:30

--- incl/config.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_HPP
-#define CONFIG_HPP
-
-const int POPULATION=100;
-const int TIMES=100;
-const double PR_CROSS=0.6;
-const double PR_MUTATION=0.05;
-
-#endif
--- incl/define.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef DEFINE_HPP
-#define DEFINE_HPP
-
-void Solve();
-
-#endif
